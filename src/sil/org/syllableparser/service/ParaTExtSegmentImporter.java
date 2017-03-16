190f2c024662ebe0f3c8ec7cfe31cdaac84cebd3
andy black <andy_black@sil.org>
2017-03-16 10:49:16 -0700

    First pass at adding ParaTExt segment importer
    Basic functionality; no UI

---
+++ src/sil/org/syllableparser/service/ParaTExtSegmentImporter.java
@@ -0,0 +1,125 @@
+// Copyright (c) 2016-2017 SIL International 
+// This software is licensed under the LGPL, version 2.1 or later 
+// (http://www.gnu.org/licenses/lgpl-2.1.html) 
+/**
+ * 
+ */
+package sil.org.syllableparser.service;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Iterator;
+import java.util.Optional;
+import java.util.ResourceBundle;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Stream;
+
+import org.controlsfx.control.StatusBar;
+
+import javafx.collections.ObservableList;
+import javafx.concurrent.Task;
+import javafx.scene.Cursor;
+import javafx.scene.Scene;
+import sil.org.syllableparser.Constants;
+import sil.org.syllableparser.MainApp;
+import sil.org.syllableparser.model.LanguageProject;
+import sil.org.syllableparser.model.Segment;
+import sil.org.syllableparser.model.SylParserObject;
+import sil.org.syllableparser.model.cvapproach.CVNaturalClass;
+import sil.org.syllableparser.model.cvapproach.CVSegmentOrNaturalClass;
+import sil.org.syllableparser.view.ControllerUtilities;
+
+/**
+ * @author Andy Black
+ *
+ */
+public class ParaTExtSegmentImporter extends SegmentImporter {
+
+	final String PARATEXT_CHARACTERS_PARAMETER_SECTION_MARKER = "[Characters]";
+
+	public ParaTExtSegmentImporter() {
+		super();
+	}
+
+	public ParaTExtSegmentImporter(LanguageProject languageProject) {
+		super(languageProject);
+	}
+
+	@Override
+	public void importSegments(File file) throws SegmentImporterException {
+		try {
+			InputStreamReader reader = new InputStreamReader(new FileInputStream(file),
+					Constants.UTF8_ENCODING);
+			BufferedReader bufr = new BufferedReader(reader);
+
+			String line = findStartOfCharacterDefinitions(bufr);
+			if (line == null) {
+				bufr.close();
+				throw new ParaTExtSegmentImporterNoCharactersException(file.getPath());
+			}
+			line = bufr.readLine(); // eat the marker
+			removeAllSegments();
+			line = createSegmentsFromParaTExtDefinition(bufr, line);
+			// We do not care about what else is in the file, so just close it
+			bufr.close();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	}
+
+	protected String findStartOfCharacterDefinitions(BufferedReader bufr) throws IOException {
+		String line = bufr.readLine();
+		while (line != null && !line.equals(PARATEXT_CHARACTERS_PARAMETER_SECTION_MARKER)) {
+			line = bufr.readLine();
+		}
+		return line;
+	}
+
+	protected String createSegmentsFromParaTExtDefinition(BufferedReader bufr, String line)
+			throws IOException {
+		while (line != null && !line.matches("^\\[.+\\]$")) {
+			if (line.length() > 0) {
+				int indexOfStartOfCharacters = line.indexOf("=") + 1;
+				String[] chars = line.substring(indexOfStartOfCharacters).split("/");
+				if (chars.length <= 0) {
+					continue;
+				}
+				String sName = chars[0];
+				StringBuilder sb = new StringBuilder();
+				for (int i = 0; i < chars.length; i++) {
+					sb.append(chars[i]);
+					sb.append(" ");
+				}
+				String sGraphemes = sb.toString().trim();
+				Segment seg = new Segment(sName, sGraphemes, "");
+				languageProject.getSegmentInventory().add(seg);
+			}
+			line = bufr.readLine();
+		}
+		return line;
+	}
+
+	protected void removeAllSegments() {
+		for (CVNaturalClass nc : languageProject.getCVApproach().getCVNaturalClasses()) {
+			// We use a list and a simple for loop to avoid getting a 
+			// java.util.ConcurrentModificationException
+			ObservableList<SylParserObject> list = nc.getSegmentsOrNaturalClasses();
+			for (int i = 0; i < list.size(); i++) {
+				SylParserObject snc = list.get(i);
+				if (snc instanceof Segment) {
+					list.remove(i);
+					i--;
+				}
+			}
+		}
+		languageProject.getSegmentInventory().clear();
+	}
+}
---
+++ test/sil/org/syllableparser/service/SegmentImporterTest.java
@@ -0,0 +1,187 @@
+// Copyright (c) 2016 SIL International 
+// This software is licensed under the LGPL, version 2.1 or later 
+// (http://www.gnu.org/licenses/lgpl-2.1.html) 
+/**
+ * 
+ */
+package sil.org.syllableparser.service;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import javafx.collections.ObservableList;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import sil.org.syllableparser.Constants;
+import sil.org.syllableparser.backendprovider.XMLBackEndProvider;
+import sil.org.syllableparser.model.LanguageProject;
+import sil.org.syllableparser.model.Segment;
+import sil.org.syllableparser.model.Word;
+import sil.org.syllableparser.model.cvapproach.CVNaturalClass;
+import sil.org.utility.StringUtilities;
+
+/**
+ * @author Andy Black
+ *
+ */
+public class SegmentImporterTest {
+	LanguageProject languageProject;
+	ResourceBundle bundle;
+
+	final String sParaTExtParametersNoCharactersFileName = "test/sil/org/syllableparser/testData/ParaTextParametersNoChars.lds";
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		languageProject = new LanguageProject();
+		Locale locale = new Locale("en");
+		XMLBackEndProvider xmlBackEndProvider = new XMLBackEndProvider(languageProject, locale);
+		File file = new File("src/sil/org/syllableparser/resources/starterFile.ashedata");
+		xmlBackEndProvider.loadLanguageDataFromFile(file);
+		bundle = ResourceBundle.getBundle(Constants.RESOURCE_LOCATION);
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void ImportParaTExtSegmentsFullTest() throws SegmentImporterException {
+		String sFileName = "test/sil/org/syllableparser/testData/ParaTextParametersFull.lds";
+		assertEquals(25, languageProject.getSegmentInventory().size());
+		assertEquals(3, languageProject.getCVApproach().getCVNaturalClasses().size());
+		ParaTExtSegmentImporter importer = new ParaTExtSegmentImporter(languageProject);
+		File file = new File(sFileName);
+		importer.importSegments(file);
+		assertEquals(33, languageProject.getSegmentInventory().size());
+		ObservableList<Segment> segments = languageProject.getSegmentInventory();
+		Segment segment = segments.get(0);
+		assertEquals("a", segment.getSegment());
+		assertEquals("a A á Á", segment.getGraphemes());
+		assertEquals("", segment.getDescription());
+		segment = segments.get(1);
+		assertEquals("b", segment.getSegment());
+		assertEquals("b B", segment.getGraphemes());
+		assertEquals("", segment.getDescription());
+		segment = segments.get(18);
+		assertEquals("o", segment.getSegment());
+		assertEquals("o O ó Ó", segment.getGraphemes());
+		assertEquals("", segment.getDescription());
+		segment = segments.get(32);
+		assertEquals("'", segment.getSegment());
+		assertEquals("'", segment.getGraphemes());
+		assertEquals("", segment.getDescription());
+		// Natural classes should no longer have any segments
+		ObservableList<CVNaturalClass> ncs = languageProject.getCVApproach().getCVNaturalClasses();
+		assertEquals(3, ncs.size());
+		CVNaturalClass nc = ncs.get(0);
+		assertEquals(1, nc.getSegmentsOrNaturalClasses().size());
+		nc = ncs.get(1);
+		assertEquals(0, nc.getSegmentsOrNaturalClasses().size());
+		nc = ncs.get(2);
+		assertEquals(0, nc.getSegmentsOrNaturalClasses().size());
+	}
+
+	@Test
+	public void ImportParaTExtSegmentsAtStartTest() throws SegmentImporterException {
+		String sFileName = "test/sil/org/syllableparser/testData/ParaTextParametersCharsAtStart.lds";
+		assertEquals(25, languageProject.getSegmentInventory().size());
+		assertEquals(3, languageProject.getCVApproach().getCVNaturalClasses().size());
+		ParaTExtSegmentImporter importer = new ParaTExtSegmentImporter(languageProject);
+		File file = new File(sFileName);
+		importer.importSegments(file);
+		assertEquals(33, languageProject.getSegmentInventory().size());
+		ObservableList<Segment> segments = languageProject.getSegmentInventory();
+		Segment segment = segments.get(0);
+		assertEquals("a", segment.getSegment());
+		assertEquals("a A á Á", segment.getGraphemes());
+		assertEquals("", segment.getDescription());
+		segment = segments.get(1);
+		assertEquals("b", segment.getSegment());
+		assertEquals("b B", segment.getGraphemes());
+		assertEquals("", segment.getDescription());
+		segment = segments.get(18);
+		assertEquals("o", segment.getSegment());
+		assertEquals("o O ó Ó", segment.getGraphemes());
+		assertEquals("", segment.getDescription());
+		segment = segments.get(32);
+		assertEquals("'", segment.getSegment());
+		assertEquals("'", segment.getGraphemes());
+		assertEquals("", segment.getDescription());
+		// Natural classes should no longer have any segments
+		ObservableList<CVNaturalClass> ncs = languageProject.getCVApproach().getCVNaturalClasses();
+		assertEquals(3, ncs.size());
+		CVNaturalClass nc = ncs.get(0);
+		assertEquals(1, nc.getSegmentsOrNaturalClasses().size());
+		nc = ncs.get(1);
+		assertEquals(0, nc.getSegmentsOrNaturalClasses().size());
+		nc = ncs.get(2);
+		assertEquals(0, nc.getSegmentsOrNaturalClasses().size());
+	}
+
+	@Test
+	public void ImportParaTExtSegmentsAtEndTest() throws SegmentImporterException {
+		String sFileName = "test/sil/org/syllableparser/testData/ParaTextParametersCharsAtEnd.lds";
+		assertEquals(25, languageProject.getSegmentInventory().size());
+		assertEquals(3, languageProject.getCVApproach().getCVNaturalClasses().size());
+		ParaTExtSegmentImporter importer = new ParaTExtSegmentImporter(languageProject);
+		File file = new File(sFileName);
+		importer.importSegments(file);
+		assertEquals(33, languageProject.getSegmentInventory().size());
+		ObservableList<Segment> segments = languageProject.getSegmentInventory();
+		Segment segment = segments.get(0);
+		assertEquals("a", segment.getSegment());
+		assertEquals("a A á Á", segment.getGraphemes());
+		assertEquals("", segment.getDescription());
+		segment = segments.get(1);
+		assertEquals("b", segment.getSegment());
+		assertEquals("b B", segment.getGraphemes());
+		assertEquals("", segment.getDescription());
+		segment = segments.get(18);
+		assertEquals("o", segment.getSegment());
+		assertEquals("o O ó Ó", segment.getGraphemes());
+		assertEquals("", segment.getDescription());
+		segment = segments.get(32);
+		assertEquals("'", segment.getSegment());
+		assertEquals("'", segment.getGraphemes());
+		assertEquals("", segment.getDescription());
+		// Natural classes should no longer have any segments
+		ObservableList<CVNaturalClass> ncs = languageProject.getCVApproach().getCVNaturalClasses();
+		assertEquals(3, ncs.size());
+		CVNaturalClass nc = ncs.get(0);
+		assertEquals(1, nc.getSegmentsOrNaturalClasses().size());
+		nc = ncs.get(1);
+		assertEquals(0, nc.getSegmentsOrNaturalClasses().size());
+		nc = ncs.get(2);
+		assertEquals(0, nc.getSegmentsOrNaturalClasses().size());
+	}
+
+	@Test
+	public void ImportParaTExtSegmentsNoCharsTest() {
+		try {
+			// the ParaTExt parameters file has no character definitions
+			File file = new File(sParaTExtParametersNoCharactersFileName);
+			ParaTExtSegmentImporter importer = new ParaTExtSegmentImporter(languageProject);
+			importer.importSegments(file);
+			assertEquals(25, languageProject.getSegmentInventory().size());
+		} catch (SegmentImporterException e) {
+			if (e instanceof ParaTExtSegmentImporterNoCharactersException) {
+				ParaTExtSegmentImporterNoCharactersException ptex = (ParaTExtSegmentImporterNoCharactersException) e;
+				assertEquals(sParaTExtParametersNoCharactersFileName,
+						StringUtilities.adjustForWindowsFileSeparator(ptex.getsFileName()));
+				assertEquals(25, languageProject.getSegmentInventory().size());
+			}
+		}
+	}
+}
