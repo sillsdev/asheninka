8ac16cf6a12d4096a435f003e1a1682886b4f66d
andy black <andy_black@sil.org>
2017-11-22 16:55:42 -0600

    Get Windows 64 version to build

--- src/sil/org/syllableparser/view/EnvironmentsController.java
+++ src/sil/org/syllableparser/view/EnvironmentsController.java
@@ -6,9 +6,6 @@
  */
 package sil.org.syllableparser.view;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-
 import java.io.IOException;
 import java.net.URL;
 import java.util.Comparator;
